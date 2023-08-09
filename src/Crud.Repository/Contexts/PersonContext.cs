8bc9ae16598f05a2eff241d4538ff059295b0028
Rafael Sodré <rafael.sodre@live.com>
2023-08-09 14:33:25 -0300

    Begin project

--- src/Crud.Repository/Contexts/PersonContext.cs
+++ src/Crud.Repository/Contexts/PersonContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Crud.Domain.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace Crud.Repository.Contexts
 {
-    internal class PersonContext
+    public class PersonContext : DbContext
     {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True");
+        }
+
     }
 }
